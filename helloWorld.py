37fe90a71a6134b6ba4871fe16820f684c515bc6
HadarTal <hadar.tal.isr@gmail.com>
2020-07-19 20:06:27 +0300

    proccess

--- helloWorld.py
+++ helloWorld.py
@@ -9,8 +9,6 @@ import csv
 from pandas.tseries.holiday import USFederalHolidayCalendar
 
 
-
-
 def get_date_format(days_to_sub):
     x = datetime.datetime.today() - datetime.timedelta(days=days_to_sub)
     x = x.strftime('%Y%m%d')[2:] + '00'
@@ -19,7 +17,6 @@ def get_date_format(days_to_sub):
 
 def get_date_format_from_date(date):
     return date.strftime('%Y%m%d')[2:] + '00'
-
 
 
 def get_treasury_url(days_to_sub):
@@ -37,10 +34,10 @@ def get_treasury_delta(days_to_sub):
         worksheet = workbook.sheet_by_index(0)
         open = worksheet.cell_value(rowx=6, colx=3)
         close = worksheet.cell_value(rowx=6, colx=2)
-        delta = close-open
+        delta = close - open
     except:
         delta = 0
-        print("error in get_treasury_delta: "+days_to_sub+" days_to_sub.")
+        print("error in get_treasury_delta: " + days_to_sub + " days_to_sub.")
     finally:
         return delta
 
@@ -50,7 +47,7 @@ def get_treasury_list(days_to_sub):
     with open('.idea/treasuryData.json', 'r') as f:
         data = json.load(f)
         data = json.loads(data)
-    for i in range(3, days_to_sub+3):
+    for i in range(3, days_to_sub + 3):
         my_date = get_date_format(i)
         has_date = False
         for item in data:
@@ -74,14 +71,13 @@ def get_snp_url():
     july_eighteen_period1 = 1563408000
     today = datetime.datetime.today()
     ref = datetime.datetime(2020, 7, 18)
-    dif = days_between(today, ref)-1
-    delta_to_add = 86400*dif
-    period1 = july_eighteen_period1+delta_to_add
-    period2 = july_eighteen_period2+delta_to_add
+    dif = days_between(today, ref) - 1
+    delta_to_add = 86400 * dif
+    period1 = july_eighteen_period1 + delta_to_add
+    period2 = july_eighteen_period2 + delta_to_add
     url = 'https://query1.finance.yahoo.com/v7/finance/download/%5EGSPC?period1=' + str(period1) + \
           '&period2=' + str(period2) + '&interval=1d&events=history'
     return url
-
 
 
 def get_snp_list():
@@ -92,60 +88,61 @@ def get_snp_list():
     for idx, element in enumerate(my_list):
         my_list[idx] = list(element.split(","))
         date = my_list[idx][0]
-        date = str(date[2:4])+str(date[5:7])+str(date[8:10])+'00'
-        delta = round(float(my_list[idx][4])-float(my_list[idx][1]), 2)
+        date = str(date[2:4]) + str(date[5:7]) + str(date[8:10]) + '00'
+        delta = round(float(my_list[idx][4]) - float(my_list[idx][1]), 2)
         my_list[idx] = {'date': date, 'delta': delta}
     return my_list
 
 
 def get_maturity_between_dates(d1, m1, y1, d2, m2, y2):
-    url = "https://www.treasurydirect.gov/TA_WS/securities/jqsearch?format=jsonp&callback="+ \
-        "jQuery1102034414013094282625_1595068631639&maturityDateoperator=and&filtervalue0="+ \
-        m1+"%2F"+d1+"%2F"+y1+"&filtercondition0=GREATER_THAN_OR_EQUAL"+ \
-        "&filteroperator0=0&filterdatafield0=maturityDate&filtervalue1="+m2+"%2F"+d2+"%2F"+y2+\
-        "&filtercondition1=LESS_THAN_OR_EQUAL"+ \
-        "&filteroperator1=0&filterdatafield1=maturityDate&filterscount=2&groupscount=0&pagenum="+ \
-        "0&pagesize=100&recordstartindex=0&recordendindex=100&_=1595068644528"
+    url = "https://www.treasurydirect.gov/TA_WS/securities/jqsearch?format=jsonp&callback=" + \
+          "jQuery1102034414013094282625_1595068631639&maturityDateoperator=and&filtervalue0=" + \
+          m1 + "%2F" + d1 + "%2F" + y1 + "&filtercondition0=GREATER_THAN_OR_EQUAL" + \
+          "&filteroperator0=0&filterdatafield0=maturityDate&filtervalue1=" + m2 + "%2F" + d2 + "%2F" + y2 + \
+          "&filtercondition1=LESS_THAN_OR_EQUAL" + \
+          "&filteroperator1=0&filterdatafield1=maturityDate&filterscount=2&groupscount=0&pagenum=" + \
+          "0&pagesize=100&recordstartindex=0&recordendindex=100&_=1595068644528"
+    print(url)
     str = requests.get(url).text
     i = str.index("(")
-    str = str[i+1:-2]
+    str = str[i + 1:-2]
     obj = json.loads(str)
     maturities = list()
     for obj in obj['securityList']:
         maturity_date = get_date_format_for_treasurydirect(obj['maturityDate'])
-        maturities.append({'cusip': obj['cusip'], 'offeringAmount': obj['offeringAmount'],\
+        maturities.append({'cusip': obj['cusip'], 'offeringAmount': obj['offeringAmount'], \
                            'date': maturity_date})
     maturities_by_date = list()
     for maturity in maturities:
         search_result = [x for x in maturities_by_date if x['date'] == maturity['date']]
-        if len(search_result) == 0 :
+        if len(search_result) == 0:
             maturities_by_date.append(maturity)
         else:
             search_result[0]['offeringAmount'] = int(search_result[0]['offeringAmount']) + \
-                int(maturity['offeringAmount'])
+                                                 int(maturity['offeringAmount'])
     return maturities_by_date
 
 
 def get_issues_between_dates(d1, m1, y1, d2, m2, y2):
-    url = "https://www.treasurydirect.gov/TA_WS/securities/jqsearch?format=jsonp&callback="+ \
-        "jQuery110202820668335587917_1595080545383&issueDateoperator=and&filtervalue0="+ m1 +"%2F"+d1+"%2F"+y1+\
-        "&filtercondition0=GREATER_THAN_OR_EQUAL&filteroperator0=0&filterdatafield0=issueDate&"+\
-        "filtervalue1="+m2+"%2F"+d2+"%2F"+y2+"&filtercondition1=LESS_THAN_OR_EQUAL&filteroperator1=0&"+ \
-        "filterdatafield1=issueDate&filterscount=2&groupscount=0&pagenum=0&pagesize=100&"+ \
-        "recordstartindex=0&recordendindex=100&_=1595080553625"
+    url = "https://www.treasurydirect.gov/TA_WS/securities/jqsearch?format=jsonp&callback=" + \
+          "jQuery110202820668335587917_1595080545383&issueDateoperator=and&filtervalue0=" + m1 + "%2F" + d1 + "%2F" + y1 + \
+          "&filtercondition0=GREATER_THAN_OR_EQUAL&filteroperator0=0&filterdatafield0=issueDate&" + \
+          "filtervalue1=" + m2 + "%2F" + d2 + "%2F" + y2 + "&filtercondition1=LESS_THAN_OR_EQUAL&filteroperator1=0&" + \
+          "filterdatafield1=issueDate&filterscount=2&groupscount=0&pagenum=0&pagesize=100&" + \
+          "recordstartindex=0&recordendindex=100&_=1595080553625"
     str = requests.get(url).text
     i = str.index("(")
-    str = str[i+1:-2]
+    str = str[(i + 1):-2]
     obj = json.loads(str)
     issues = list()
     for obj in obj['securityList']:
         issue_date = get_date_format_for_treasurydirect(obj['issueDate'])
         issues.append({'cusip': obj['cusip'], 'offeringAmount': obj['offeringAmount'], \
-                           'date': issue_date})
+                       'date': issue_date})
     issues_by_date = list()
     for issue in issues:
         search_result = [x for x in issues_by_date if x['date'] == issue['date']]
-        if len(search_result) == 0 :
+        if len(search_result) == 0:
             issues_by_date.append(issue)
         else:
             search_result[0]['offeringAmount'] = int(search_result[0]['offeringAmount']) + \
@@ -169,27 +166,32 @@ def get_delta_issues_maturities(start_day, start_month, start_year, end_day, end
 
 
 def get_date_format_for_treasurydirect(date):
-    date = str(date[2:4])+str(date[5:7])+str(date[8:10])+"00"
+    date = str(date[2:4]) + str(date[5:7]) + str(date[8:10]) + "00"
     return date
 
 
-#region axisAndJson
+# region axisAndJson
 ...
 
-def get_date(d):
-    return d['date']
+
+def get_axis_date(d):
+    my_date_day = d['date'][4:6]
+    my_date_month = d['date'][2:4]
+    my_date_year = '20' + d['date'][0:2]
+    return datetime.date(int(my_date_year), int(my_date_month), int(my_date_day))
 
 
 def get_treasury_delta(d):
-    return d['treasury_delta']
+    return float(d['treasury_delta'])
 
 
 def get_imd(d):
-    return int(d['imd'])/1000000
+    return int(d['imd']) / 1000000
 
 
 def get_snp_delta(d):
-    return d['delta']*1000
+    return d['delta'] * 1000
+
 
 # endregion
 
@@ -208,8 +210,8 @@ def generate_dates(dr):
     dates_to_return = list()
     start = datetime.date(int(dr[2]), int(dr[1]), int(dr[0]))
     end = datetime.date(int(dr[5]), int(dr[4]), int(dr[3]))
-    delta = end-start
-    delta = delta.days+1
+    delta = end - start
+    delta = delta.days + 1
     last_date = 0
     # make sure that the first date is a legal date
     while not is_legal_day(start - datetime.timedelta(days=1)):
@@ -247,11 +249,11 @@ def update_dates_imd(ds):
     start_date = ds[0]['date']
     start_day = start_date[4:6]
     start_month = start_date[2:4]
-    start_year = '20'+start_date[0:2]
-    end_date = ds[len(ds)-1]['date']
+    start_year = '20' + start_date[0:2]
+    end_date = ds[len(ds) - 1]['date']
     end_day = end_date[4:6]
     end_month = end_date[2:4]
-    end_year = '20'+end_date[0:2]
+    end_year = '20' + end_date[0:2]
     imds = get_delta_issues_maturities(start_day, start_month, start_year, end_day, end_month, end_year)
     for date in ds:
         search_result = [x for x in imds if x['date'] == date['date']]
@@ -265,42 +267,64 @@ def update_dates_imd(ds):
                     date['imd'] = 0
                     my_date_day = date['date'][4:6]
                     my_date_month = date['date'][2:4]
-                    my_date_year = '20'+date['date'][0:2]
-                    my_date = datetime.date(my_date_year, my_date_month, my_date_day) + + datetime.timedelta(days=1)
+                    my_date_year = '20' + date['date'][0:2]
+                    my_date = datetime.date(my_date_year, my_date_month, my_date_day) + datetime.timedelta(days=1)
                     date = [d for d in ds if d['date'] == get_date_format_from_date(my_date)][0]
         else:
             date['imd'] = 0
 
+
+# date in my format - returns datetime
+def add_days(date, days_to_add):
+    my_date_day = date[4:6]
+    my_date_month = date[2:4]
+    my_date_year = '20' + date[0:2]
+    date = datetime.date(int(my_date_year), int(my_date_month), int(my_date_day)) + datetime.timedelta(days=days_to_add)
+    return date
+
+
 # The process (main)
-date_range = ['05', '07', '2020', '19', '07', '2020']
+date_range = ['01', '07', '2020', '25', '07', '2020']
 # input('please insert the wanted date range in the following format: dd mm yyyy dd mm yyyy\n').split(' ')
 dates = generate_dates(date_range)
+minDate = add_days(dates[0]['date'], -2)
+maxDate = add_days(dates[len(dates) - 1]['date'], 2)
 update_dates_treasury_delta(dates)
-# update_dates_imd(dates)
+update_dates_imd(dates)
 print(dates)
 
-
-# plt
-fig, ax = plt.subplots()
-
 # plt - x axis
-plt.axhline(y=0, color='black', linestyle='-')
+fig, ax = plt.subplots()
+ax.axhline(y=0, color='black', linestyle='-')
+plt.xlabel('date')
+ax.set_xlim([minDate, maxDate])
 
+# calculate dates
 illegal_dates = [x for x in dates if x['is_legal_date'] is False]
 legal_dates = [x for x in dates if x['is_legal_date'] is True]
 
 # plt - illegal_dates
-ax.scatter(list(map(get_date, illegal_dates)), list(map((lambda x: 0), illegal_dates)), \
-           marker='o', color='yellow')
+plt.scatter(list(map(get_axis_date, illegal_dates)), list(map((lambda x: 0), illegal_dates)),
+            marker='o', color='#a832a4')
 
 # plt - treasury_delta
-ax.plot(list(map(get_date, treasury_data)), list(map(get_delta, treasury_data)))
-ax.scatter(list(map(get_date, treasury_data)), list(map(get_delta, treasury_data)), marker='o', color='red')
-for n in treasury_data:
-    ax.annotate(str(n['delta']), (n['date'], n['delta']))
+plt.plot(list(map(get_axis_date, legal_dates)), list(map(get_treasury_delta, legal_dates)),
+         color='blue', linestyle='-', label='treasury_delta')
+plt.scatter(list(map(get_axis_date, legal_dates)), list(map(get_treasury_delta, legal_dates)), marker='o', color='blue')
+for n in legal_dates:
+    plt.annotate(str(n['treasury_delta']), (get_axis_date(n), n['treasury_delta']))
 
+# plt - imd
+ax.plot(list(map(get_axis_date, legal_dates)), list(map(get_imd, legal_dates)),
+        color='green', linestyle='--', label='imd')
+ax.scatter(list(map(get_axis_date, legal_dates)), list(map(get_imd, legal_dates)),
+           marker='o', color='green')
+for n in legal_dates:
+    ax.annotate(str(get_imd(n)), (get_axis_date(n), int(get_imd(n))))
 
 # plt - show
+plt.grid(True)
+plt.legend()
 plt.show()
 
 """
@@ -373,7 +397,6 @@ for n in treasury_data:
     ax.annotate(str(n['delta']), (n['date'], n['delta']))
 """
 
-
 print('thank you')
 
 """
@@ -416,7 +439,3 @@ for n in snp_data:
     ax.annotate(str(n['delta']), (n['date'], n['delta']*1000))
     
 """
-
-
-
-
